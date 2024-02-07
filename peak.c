13f54811eccd5326c987d099c89767555ba42413
ashishg2004 <98903125+ashishg2004@users.noreply.github.com>
2024-02-07 19:52:37 +0530

    Add files via upload

---
+++ peak.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int n,temp; 
+    printf("enter no. of elements in an array");
+    scanf("%d",&n);
+    int arr[100];
+    printf("enter elements in an array");
+    for(int i=0;i<n;i++)
+            scanf("%d",&arr[i]);
+    // sorting the array using bubble sort algorithm
+    for(int j = 0 ; j < n ; j++ )
+    {
+        for(int k=0;k<n-j-1;k++)
+        {
+    if (arr[j]>arr[j+1])
+    {
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+            }              
+        }   
+      }
+    printf("maximum element = %d",arr[n-1]);
+
+}
